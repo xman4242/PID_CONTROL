2defc47313aa07d0a87408100e10a74afb416ef2
xman4242 <43799507+xman4242@users.noreply.github.com>
2019-05-05 11:46:03 -0500

    Worked on the basic math

--- PID_CONTROL.cpp
+++ PID_CONTROL.cpp
@@ -1,1 +1,14 @@
 #include "PID_CONTROL.h"
+
+void PID_CONTROL::runPID(int Setpoint, int To_Set, int SensorInput, int Kp, int Ki, int Kd, bool Reversed)
+{
+    //Read Output
+    ErrorNum = Setpoint - SensorInput;
+    //Calculate New Value 
+    Integral = Integral + ErrorNum * 20;
+    Derivative = (ErrorNum - PreviousError)/20;
+    //Output the New Value
+    FinalOutput = Kp * ErrorNum + Ki * Integral + Kd * Derivative;
+    //Set the previous error
+    PreviousError = ErrorNum;
+}
--- PID_CONTROL.h
+++ PID_CONTROL.h
@@ -5,13 +5,17 @@ class PID_CONTROL
 {
 
 public:
-
-
+//Vars
+int ErrorNum = 0;
+int Integral = 0;
+int Derivative = 0;
+int PreviousError = 0;
+int FinalOutput = 0;
 //Functions
- 
+void runPID(int Setpoint, int To_Set, int SensorInput, int Kp, int Ki, int Kd, bool Reversed);
 
 private:
   
 };
 
-#endif /* _YETI_YUKON_H_ */
+#endif /* _PID_CONTROL_H_ */
